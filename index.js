0f78ba2b799adecc1242a42b8b91435c09afd6b3
Vanskater362 <wag12011@byui.edu>
2018-11-28 19:25:31 -0600

    ordered leader board by points

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ express()
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
-      const result = await client.query('SELECT username, wins, losses, draws FROM players INNER JOIN record ON players.id = record.playerID');
+      const result = await client.query('SELECT username, wins, losses, draws, points FROM players INNER JOIN record ON players.id = record.playerID ORDER BY points');
       //const result = await client.query('SELECT username FROM players');
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
