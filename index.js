6e80a0de3448b4383b7135d9b2bba6026d8908b0
Vanskater362 <wag12011@byui.edu>
2018-11-28 22:03:06 -0600

    working on regestering users

--- index.js
+++ index.js
@@ -28,14 +28,14 @@ express()
     }
   })
   .get('/register', function (req, res){
-    res.send('Username: ' + req.body.username + ' Password: ' + req.body.password );
-    /*const query = 'SELECT username, password FROM players WHERE username = ' + req.body.username;
+    //res.send('Username: ' + req.body.username + ' Password: ' + req.body.password );
+    //const query = 'SELECT username, password FROM players WHERE username = ' + req.body.username;
     const client = await pool.connect()
-    const result = await client.query(query);
+    const result = await client.query({text: 'SELECT username, password FROM players WHERE username = $1 AND password = $2', values: [req.body.username, req.body.password],});
     const results = { 'results': (result) ? result.rows : null};
     res.render('pages/db', results );
     res.send(results);
-    client.release();*/
+    client.release();
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   
