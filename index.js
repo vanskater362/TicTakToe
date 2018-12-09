e694cd2ec1548ec15f6fce2d1f23a650c20035fd
Vanskater362 <wag12011@byui.edu>
2018-12-08 22:15:14 -0600

    working on updating home page after db access

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ express()
         console.log("Fail User doesn't match");
       }
       else {
-        /*bcrypt.compare(password, result[0].password, function(err, res){
+        bcrypt.compare(password, result[0].password, function(err, res){
           if(!res) {
             response.json({success: false});
             console.log("Fail: Password doesn't match");
@@ -82,7 +82,7 @@ express()
             req.session.user = req.body.username;
             response.json({success: true});
           }
-        });*/
+        });
         result = {success: true};
         console.log(password);
         console.log("Success!");
